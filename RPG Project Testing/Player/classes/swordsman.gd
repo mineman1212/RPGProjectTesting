6ce7678133c3382a24e4099c30af84caa487c0df
TeddyManGuy <77462538+kadenmork@users.noreply.github.com>
2023-12-04 15:47:54 -0600

    Added some simple animation to the swordsman

--- RPG Project Testing/Player/classes/swordsman.gd
+++ RPG Project Testing/Player/classes/swordsman.gd
@@ -2,12 +2,36 @@ extends CharacterBody2D
 
 @export var max_speed: int = 250
 var speed = max_speed
-
+var attackCD: int = 3
+var canAttack: bool = true
 
 func _process(_delta):
-	#Keyboard Input
+	#Keyboard Input direction
 	var direction = Input.get_vector("left", "right", "up", "down")
+	#setting move speed and direction
 	velocity = direction * speed
+	#making the movement
 	move_and_slide()
 
 
+
+#Sword Swing Animation
+	if Input.is_action_pressed("primary") and canAttack == true:
+		#Create Attack
+		
+		#Play Attack Animation
+		var tween = create_tween()
+		tween.tween_property($Sprite2D, "frame", 23, 0.6).from(18)
+		tween.tween_property($Sprite2D, "frame", 5, 0.8).from(0)
+		
+		#Start Attack CD
+		canAttack = false
+		$primaryCD.start()
+		
+		
+
+
+
+
+func _on_primary_cd_timeout():
+	canAttack = true
