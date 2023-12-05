4f88be66f875434a8c62cd7e0947d63db2ceeb00
TeddyManGuy <77462538+kadenmork@users.noreply.github.com>
2023-12-05 10:53:40 -0600

    Added breathing animation, and matched character direction to movement

--- RPG Project Testing/Player/classes/swordsman.gd
+++ RPG Project Testing/Player/classes/swordsman.gd
@@ -4,26 +4,41 @@ extends CharacterBody2D
 var speed = max_speed
 var attackCD: int = 3
 var canAttack: bool = true
+var breath: bool = true
+
 
 func _process(_delta):
 	#Keyboard Input direction
 	var direction = Input.get_vector("left", "right", "up", "down")
 	#setting move speed and direction
 	velocity = direction * speed
-	#making the movement
+	#acting the movement
 	move_and_slide()
 
+	#breathing animation
+	if breath == true:
+		breath = false
+		$breath.start()
+		var statictween = create_tween()
+		statictween.tween_property($CharacterSprite, "frame", 5, 0.8).from(0)
+
+#Changing PNG direction depending on movement.
+	if Input.is_action_pressed("left"):
+		$CharacterSprite.scale = Vector2(-1, 1)
+
+	if Input.is_action_pressed("right"):
+		$CharacterSprite.scale = Vector2(1, 1)
 
 
-#Sword Swing Animation
+#Primary Attack
 	if Input.is_action_pressed("primary") and canAttack == true:
+		breath = false
+		
 		#Create Attack
 		
 		#Play Attack Animation
 		var tween = create_tween()
-		tween.tween_property($Sprite2D, "frame", 23, 0.6).from(18)
-		tween.tween_property($Sprite2D, "frame", 5, 0.8).from(0)
-		
+		tween.tween_property($CharacterSprite, "frame", 23, 0.6).from(18)
 		#Start Attack CD
 		canAttack = false
 		$primaryCD.start()
@@ -32,6 +47,11 @@ func _process(_delta):
 
 
 
-
+#Primary Attack CD
 func _on_primary_cd_timeout():
 	canAttack = true
+	breath = true
+
+#Breath Animation CD
+func _on_breath_timeout():
+	breath = true
